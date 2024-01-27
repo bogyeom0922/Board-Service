d1486219a7cf5a3a9de560b04140ad1370a79a37
bogyeom0922 <pbk0398@gmail.com>
2024-01-27 15:58:34 +0900

    setting

---
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,10 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FirstController {
+    public String niceToMeetYou() {
+        return "greetings";
+    }
+}
